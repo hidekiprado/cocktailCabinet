a6a7c9517fb4377c27301f6bbeea8fe9a300dcb5
Vinicius Prado <1viniciusprado@gmail.com>
2023-07-07 11:45:27 +1000

    Vercel settings - test 1

--- db/db.js
+++ db/db.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const db = new pg.Pool({
   connectionString:
-    process.env.PG_ADDRESS || "cocktail_cabinet" || process.env.POSTGRES_URL,
+    process.env.PG_ADDRESS ||
+    "cocktail_cabinet" ||
+    process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = db;
