1bb68c7bca8bd1b843cdc93795a6ae96739da093
Vinicius Prado <1viniciusprado@gmail.com>
2023-07-07 11:32:51 +1000

    Vercel settings

--- db/db.js
+++ db/db.js
@@ -2,7 +2,8 @@ const pg = require("pg");
 require("dotenv").config();
 
 const db = new pg.Pool({
-  connectionString: process.env.PG_ADDRESS || "cocktail_cabinet",
+  connectionString:
+    process.env.PG_ADDRESS || "cocktail_cabinet" || process.env.POSTGRES_URL,
 });
 
 module.exports = db;
