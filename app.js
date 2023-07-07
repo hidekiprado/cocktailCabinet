a73a7169de9bdf5cdab15e69de41e095b84442bd
Vinicius Prado <1viniciusprado@gmail.com>
2023-07-07 12:06:19 +1000

    VERCEL FREE HOST - test2

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ const usersController = require("./controller/users");
 
 const app = express();
 
+const { v4 } = require("uuid");
+//VERCEL FREE HOST
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
+//VERCEL FREE HOST
+
 const port = process.env.PORT || 3000;
 app.use(express.static("client"));
 app.use(express.json());
