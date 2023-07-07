36eac48bb8be0c74400b52044d4926f907b09bc3
Vinicius Prado <1viniciusprado@gmail.com>
2023-07-07 11:05:44 +1000

    Ready to be hosted

--- db/db.js
+++ db/db.js
@@ -1,12 +1,8 @@
 const pg = require("pg");
+require("dotenv").config();
 
 const db = new pg.Pool({
-  connectionString:
-    "postgres://postgres:223jl23l@localhost:5432/cocktail_cabinet" ||
-    "cocktail_cabinet",
+  connectionString: process.env.PG_ADDRESS || "cocktail_cabinet",
 });
-// const db = new pg.Pool({
-//   connectionString: process.env.PG_ADDRESS || "cocktail_cabinet",
-// });
 
 module.exports = db;
