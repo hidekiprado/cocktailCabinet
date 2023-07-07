7a5732d73b66fc40926b968ed2d5173442ea659b
Vinicius Prado <1viniciusprado@gmail.com>
2023-07-07 12:15:14 +1000

    Vercel - settings test2

--- app.js
+++ app.js
@@ -6,24 +6,8 @@ const usersController = require("./controller/users");
 
 const app = express();
 
-//VERCEL FREE HOST
-const { v4 } = require("uuid");
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
-//VERCEL FREE HOST
-
 const port = process.env.PORT || 3000;
+
 app.use(express.static("client"));
 app.use(express.json());
 
